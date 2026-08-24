8d606ee661e58d46949d5c4060c4163749b7f92f
Sreejoyee Saha <sahasreejoyee@gmail.com>
2026-08-24 21:47:07 +0530

    LC-longest-consecutive-sequence [Runtime: N/A; Memory: N/A]

--- union-find/longest-consecutive-sequence.py
+++ union-find/longest-consecutive-sequence.py
@@ -2,7 +2,7 @@ class Solution:
     def longestConsecutive(self, nums: List[int]) -> int:
         nums_set = set(nums)
         longest = 0
-        for i in nums:
+        for i in nums_set:
             if (i - 1) not in nums_set:
                 length = 0
                 while (i + length) in nums_set:
