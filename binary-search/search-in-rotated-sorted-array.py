4e50e12d9fff161a8efd8d803aa94cba8927d0d3
Sreejoyee Saha <sahasreejoyee@gmail.com>
2026-09-18 13:10:42 +0530

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

---
+++ binary-search/search-in-rotated-sorted-array.py
@@ -0,0 +1,26 @@
+class Solution:
+    def search(self, nums: list[int], target: int) -> int:
+        low=0
+        high= len(nums)-1
+        ans=nums[0]
+        while(low <= high):
+            mid=(low+high)//2
+            if (nums[low] <= nums[high]):
+                if nums[low] == target:
+                    return low
+                
+                break
+            if (nums[low] <= nums[mid]):
+                if nums[low] == target:
+                    return low
+                    break
+                low=mid+1
+            else:
+                if nums[low] == target:
+                    return low
+                    break
+                high=mid - 1
+        return -1
+        
+       
+        
