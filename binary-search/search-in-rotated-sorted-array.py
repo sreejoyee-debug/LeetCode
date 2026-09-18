f43313264992f253e1ab51167935d7f5552b6a3a
Sreejoyee Saha <sahasreejoyee@gmail.com>
2026-09-18 13:28:37 +0530

    LC-search-in-rotated-sorted-array [Runtime: N/A; Memory: N/A]

--- binary-search/search-in-rotated-sorted-array.py
+++ binary-search/search-in-rotated-sorted-array.py
@@ -5,22 +5,22 @@ class Solution:
         ans=nums[0]
         while(low <= high):
             mid=(low+high)//2
-            if nums[low] == target:
-                return low
-            if nums[high] == target:
-                return high
             if nums[mid] == target:
                 return mid
-            if (nums[low] <= nums[mid]):
-                if nums[low] == target:
-                    return low
-                    break
-                low=mid+1
+
+            if nums[low] <= nums[mid]:
+                # left half [low..mid] is sorted
+                if nums[low] <= target < nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
             else:
-                if nums[low] == target:
-                    return low
-                    break
-                high=mid - 1
+                # right half [mid..high] is sorted
+                if nums[mid] < target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
         return -1
 
        
